f62d7c8f4bb8193e287a8d5409504c18dd1f558c
Long Nguyen <electronicdollar@gmail.com>
2018-01-08 00:46:10 +0700

    add new source code

---
+++ website/config.js
@@ -0,0 +1,18 @@
+/* CHANGE ME to real domain */
+var api = "https://pool.edollar.cash:22022";
+
+var poolHost = "pool.edollar.cash";
+
+var irc = "irc.freenode.net/#edollar";
+
+var email = "electronicdollar@gmail.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.edollar.cash/block/{id}";
+
+var transactionExplorer = "https://explorer.edollar.cash/tx/{id}";
+
+var themeCss = "themes/motherboard-dark-theme.css";
